9d391caa108a4406ee64fcb3368ddf4ad0034dcb
OmerGit <rafaelxdale@gmail.com>
2023-05-15 12:17:46 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
